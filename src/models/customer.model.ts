d34e4d5e18ef45ce88f165a2ced49385162f5d6e
rebecca-fundy <rebecca@fundysoftware.com>
2021-12-02 14:52:35 -0800

    Basic customer GET function working.

---
+++ src/models/customer.model.ts
@@ -0,0 +1,37 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Customer extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+  })
+  first_name?: string;
+
+  @property({
+    type: 'string',
+  })
+  last_name?: string;
+
+  @property({
+    type: 'date',
+  })
+  created_at?: string;
+
+
+  constructor(data?: Partial<Customer>) {
+    super(data);
+  }
+}
+
+export interface CustomerRelations {
+  // describe navigational properties here
+}
+
+export type CustomerWithRelations = Customer & CustomerRelations;
